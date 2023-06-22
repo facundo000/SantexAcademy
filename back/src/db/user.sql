b52163702a0f27badebdef64a0a6802b6e68a5d2
Facundo Nicolás Guzman Olariaga <88353632+facundo000@users.noreply.github.com>
2023-06-22 16:57:00 -0300

    Add files via upload

---
+++ back/src/db/user.sql
@@ -0,0 +1,4 @@
+/* Esta es la tabla que está dentro de la base de datos "data_base".
+Lo dejé explícitamente ya que no puedo exportar mi base de datos */
+INSERT INTO users (name, password)
+VALUES ('admin', 'admin');
